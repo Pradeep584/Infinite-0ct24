21ab78653371ed5a94595b13e704b4cc00e90891
Pradeep Nuteti <pradeepnu@infinite.com>
2024-10-31 10:49:39 +0530

    assignment

---
+++ CSharpPrograms/Assignments/Problem 1/Problem 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1==num2)
+            {
+                Console.WriteLine($"given {num1} and {num2} are equal");
+            }
+            else
+            {
+                Console.WriteLine($"Given two numbers are not equal");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpPrograms/Assignments/Problem_2/Problem_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the numbers");
+            int a = Convert.ToInt32(Console.ReadLine());
+           
+            if(a>0)
+            {
+                Console.WriteLine($"Given number is positive");
+            }
+            else
+            {
+                Console.WriteLine($"Given number is negative");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
