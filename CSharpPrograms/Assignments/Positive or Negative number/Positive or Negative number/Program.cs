855f3d7d9e19970a35baa907a38b5c2b7e083ded
Pradeep Nuteti <pradeepnu@infinite.com>
2024-10-31 08:29:22 +0530

    Assignment

---
+++ CSharpPrograms/Assignments/Positive or Negative number/Positive or Negative number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Positive_or_Negative_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a > 0)
+            {
+                Console.WriteLine("The given number is positive");
+            }
+
+            else 
+                    {
+                      Console.WriteLine("The given number negative");
+                    }
+            Console.Read();
+        }
+    }
+}
---
+++ CSharpPrograms/Assignments/equalornot/equalornot/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace equalornot
+{
+    internal class P1
+    {
+        static void Main(string[] args)
+        {
+            int a=Convert.ToInt32(Console.ReadLine());  
+            int b=Convert.ToInt32(Console.ReadLine());
+
+            if (a == b)
+            {
+                Console.WriteLine($"Given {a} and {b} numbers are equal");
+            }
+            else
+            {
+                Console.WriteLine("Given numbers are not equal");
+            }
+            Console.ReadLine(); 
+                    
+        }
+    }
+}
