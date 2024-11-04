b924d8468d855629863c76d9c454c6253e12c067
Pradeep Nuteti <pradeepnu@infinite.com>
2024-11-04 10:11:03 +0530

    Assignment

---
+++ CSharpPrograms/Assignments/Assignment_1/Problem_3/Problem_3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the numbers");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter sign");
+            char sign = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("enter input second number ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            switch (sign)
+            {
+                case '+':
+                    Console.WriteLine($"{a} + {b} ={a + b}");
+                    Console.Read();
+                    break;
+                case '-':
+                    Console.WriteLine($"{a} - {b}={a - b}");
+                    Console.Read();
+                    break;
+                case '*':
+                    Console.WriteLine($"{a} * {b}={a * b}");
+                    Console.Read();
+                    break;
+                case '/':
+                    Console.WriteLine($"{a}/{b}={ a / b}");
+                    Console.Read();
+                    break;
+                default:
+                    Console.WriteLine("invalid grade");
+                    Console.Read();
+                    break;
+            }
+            Console.ReadLine();
+
+
+            }
+    }
+}
---
+++ CSharpPrograms/Assignments/Assignment_1/Problem_4/Problem_4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i=0; i<=10; i++)
+            {
+                Console.WriteLine($"{num} * {i} = {num * i}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
