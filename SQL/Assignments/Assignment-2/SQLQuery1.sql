cbf67fe5f44d84aa3075486d19fb22b3bb92db3a
Pradeep Nuteti <pradeepnu@infinite.com>
2024-11-27 20:57:43 +0530

    Assignment-2

--- SQL/Assignments/Assignment-2/SQLQuery1.sql
+++ SQL/Assignments/Assignment-2/SQLQuery1.sql
@@ -1,6 +1,6 @@
 use Pradeep_db
-create table DEPT
 
+create table DEPT
 (
 DEPTNO int primary key not null,
 DNAME varchar(20) not null,
@@ -44,51 +44,93 @@ insert into dept values
 
  select * from EMP
 SELECT * FROM DEPT
- --1
+
+ --1 List all employees whose name begins with 'A'
  select * from emp where ename like 'a%'
- --2
+
+ --2 Select all those employees who don't have a manager
  select * from emp where MGR_ID is null
- --3
+
+ --3 List employee name, number and salary for those employees who earn in the range 1200 to 1400
  select ename,empno,sal from emp where sal between 1200 and 1400
- --11
- select ename,sal from emp where ename in('MILLER','SMITH')
- --13
- select ename, sal*12 as 'Annual Salary' from emp where ename = 'SMITH'
- --14
- select ename,sal from emp  where sal  not between 1500 and 2850
- --15
- select ename,MGR_ID,count(*) as emp_count from emp group by MGR_ID having count(*)>2
- --9
- select ename,sal from emp where job='ANALYST' and sal > 1200 and deptno=20 order by ename
- --5
- select count(*) as 'No.of Clerks' from emp where job='Clerk'
- --4
+
+ --4  Give all the employees in the RESEARCH department a 10% pay rise. Verify that this has been done by listing all their details before and after the rise. 
  select ename,sal,sal+(0.10*sal) as 'sal+10%' from emp where DEPTNO=(select deptno  from dept where dname='RESEARCH') 
- --7
+
+ --5 Find the number of CLERKS employed. Give it a descriptive heading. 
+ select count(*) as 'No.of Clerks' from emp where job='Clerk'
+
+ --6 Find the average salary for each job type and the number of people employed in each job
+ select job,avg(sal) as 'Avg salary',COUNT(*) as 'Emp count' from emp group by job
+ --7 List the employees with the lowest and highest salary.
  select ename,sal from emp where sal=(select max(sal) from emp) union select ename,sal from emp where  sal=(select min(sal) from emp)
+
+ --8 List full details of departments that don't have any employees
+ select DNAME from DEPT left  join EMP on dept.DEPTNO=EMP.DEPTNO where emp.DEPTNO is null
+
+ --9  Get the names and salaries of all the analysts earning more than 1200 who are based in department 20. Sort the answer by ascending order of name. 
+ select ename,sal from emp where job='ANALYST' and sal > 1200 and deptno=20 order by ename
+
+ --10 For each department, list its name and number together with the total salary paid to employees in that department
+ select e1.ENAME,e1.SAL,e1.DEPTNO,sum(e1.SAL) as 'Sum salary' from emp e1 join EMP e2 on  e1.DEPTNO=e2.DEPTNO  
+ group by e1.ENAME,e1.SAL,e1.DEPTNO
+
+ --11  Find out salary of both MILLER and SMITH
+ select ename,sal from emp where ename in('MILLER','SMITH')
+
+ --12 Find out the names of the employees whose name begin with ‘A’ or ‘M’. 
+ select ename from emp where ENAME like'A%' or  ename like 'M%'
+
+ --13 Compute yearly salary of SMITH
+ select ename, sal*12 as 'Annual Salary' from emp where ename = 'SMITH'
+
+ --14 List the name and salary for all employees whose salary is not in the range of 1500 and 2850. 
+ select ename,sal from emp  where sal  not between 1500 and 2850
+
+
+ --15 Find all managers who have more than 2 employees reporting to them
+ select MGR_ID,count(*) as emp_count from emp group by  MGR_ID having count(*)>2
+ 
+ 
+ 
+ 
 
   select dname from emp,dept where emp.deptno=dept.deptno group by dept.DEPTNO having count(emp.DEPTNO)=0
 
- select * from emp e where exists (select 'x' from emp where MGR_ID=e.MGR_ID)
+ 
 
  select * from emp where sal< any
  (select sal from emp where job='salesman')
  and job<>'salesman'
 
 
- select ename,sal,DEPTNO from emp e1 where SAL>(select avg(sal) from emp e2 where e1.DEPTNO=e2.DEPTNO)
 
- --with corelated sub query
-select ename,sal,dname from emp e1,dept e2
-where sal> (select avg(sal) from emp where e2.deptno = e1.deptno)
-                --5166  bhanuprakash
-				--5200  chiramya, hareesh
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ create or alter proc sp_inc (@enum int)
+ as
+ begin
+ select empno,ename,sal,sal+100 from emp where sal<1250 
+ and empno=@enum
+ end
+
+sp_inc 7876
+
  
---with inner join
-
-select ename,sal,dname from emp,dept where emp.DEPTNO=dept.DEPTNO
-and sal>(select avg(sal) as avgsal from emp,DEPT)
-
-select * from emp e1 inner join (select deptno,avg(sal) as avgsal from emp group by deptno) as e2 on 
-e1.deptno=e2.deptno where e1.sal>e2.avgsal
-
