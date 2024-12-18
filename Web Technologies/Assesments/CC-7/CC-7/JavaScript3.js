7f14c226b3d7cf3e137180381f4e88c2eb7a371b
Pradeep Nuteti <pradeepnu@infinite.com>
2024-12-18 11:25:07 +0530

    cc_7

---
+++ Web Technologies/Assesments/CC-7/CC-7/JavaScript3.js
@@ -0,0 +1,11 @@
+﻿< !DOCTYPE html >
+    <html>
+      <button id="Button">Click Me!</button>
+      <script>
+        const button = document.getElementById("Button");
+        button.addEventListener("click", () => {
+            console.log("Button was clicked!");
+            alert("Button  clicked Successfully...!");
+        });
+      </script>
+   </html>
