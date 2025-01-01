e78bce97dcc6126a36209d6349a3d75b7ea18519
Pradeep Nuteti <pradeepnu@infinite.com>
2025-01-01 22:22:52 +0530

    assignment1

---
+++ MVC/Assignments/Assignment-1/Assignment-1/Repository/IContactRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment-1.Models;
+namespace Assignment-1.Repositories
+{
+    interface IContactRepository
+    {
+        Task<List<Contact>> GetAllAsync();
+        Task CreateAsync(Contact contact);
+        Task DeleteAsync(long Id);
+    }
+}
