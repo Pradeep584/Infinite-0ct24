1f4d1b5c42758f7abbf62d34d9c3c1c88cd7207e
Pradeep Nuteti <pradeepnu@infinite.com>
2025-01-02 20:14:47 +0530

    MVC-Cc

---
+++ MVC/Assesments/Assesment1-MVC/Assesment1-MVC/Movies.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assesment1_MVC
+{
+    public class Movies
+    {
+    }
+}
---
+++ MVC/Assesments/Assesment_MVC/Assesment_MVC/Controllers/CodeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assesment_MVC.Models;
+
+namespace Assesment_MVC.Controllers
+{
+   
+        public class CodeController : Controller
+        {
+            public northwindEntities db = new northwindEntities();
+
+
+            public ActionResult CustomersList()
+            {
+                var customers = db.Customers.Where(c => c.Country == "Germany").ToList();
+                return View(customers);
+            }
+
+
+            public ActionResult CustomerOrder()
+            {
+                var customerDetails = db.Orders
+                                         .Where(o => o.OrderID == 10248)
+                                         .Select(o => o.Customer)
+                                         .FirstOrDefault();
+                return View(customerDetails);
+            }
+        }
+    }
